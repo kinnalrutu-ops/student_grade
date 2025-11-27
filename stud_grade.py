9754a9e8a4f2c0df566c9f588884d13a76f5070a
Rutu <kinnalrutu@gmail.com>
2025-11-27 14:09:10 +0530

    Refactor command-line arguments handling for grades

--- stud_grade.py
+++ stud_grade.py
@@ -1,18 +1,29 @@
 import sys
-if len(sys.argv) != 6:
-    print("Please pass 5 subject marks as command-line arguments!")
-    sys.exit()
 
-a = (sys.argv[1])
-b = (sys.argv[2])
-c = (sys.argv[3])
-d = (sys.argv[4])
-e = (sys.argv[5])
+# If 5 arguments are passed from command line
+if len(sys.argv) == 6:
+    script_name = sys.argv[0]
+    marks1 = int(sys.argv[1])
+    marks2 = int(sys.argv[2])
+    marks3 = int(sys.argv[3])
+    marks4 = int(sys.argv[4])
+    marks5 = int(sys.argv[5])
+else:
+    # Default values
+    script_name = sys.argv[0]
+    marks1 = 80
+    marks2 = 90
+    marks3 = 89
+    marks4 = 67
+    marks5 = 56
 
-marks = [a, b, c, d, e]
+marks = [marks1, marks2, marks3, marks4, marks5]
 
+# Fail condition
 if any(m < 35 for m in marks):
     grade = "F"
+    total = sum(marks)
+    percentage = total / 5
 else:
     total = sum(marks)
     percentage = total / 5
