65fb7edd759ec7f9bec87d2f306e3809b849b7aa
Rutu <kinnalrutu@gmail.com>
2025-11-27 13:56:11 +0530

    Update variable assignments for subject marks
    Changed variable assignments to keep marks as strings.

--- stud_grade.py
+++ stud_grade.py
@@ -3,11 +3,11 @@ if len(sys.argv) != 6:
     print("Please pass 5 subject marks as command-line arguments!")
     sys.exit()
 
-a = int(sys.argv[1])
-b = int(sys.argv[2])
-c = int(sys.argv[3])
-d = int(sys.argv[4])
-e = int(sys.argv[5])
+a = (sys.argv[1])
+b = (sys.argv[2])
+c = (sys.argv[3])
+d = (sys.argv[4])
+e = (sys.argv[5])
 
 marks = [a, b, c, d, e]
 
