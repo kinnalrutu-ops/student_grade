e06826927271f85faab15fa7d4778730f9001ca7
Rutu <kinnalrutu@gmail.com>
2025-11-27 13:53:08 +0530

    Refactor stud_grade.py to accept command-line arguments

---
+++ stud_grade.py
@@ -0,0 +1,31 @@
+import sys
+if len(sys.argv) != 6:
+    print("Please pass 5 subject marks as command-line arguments!")
+    sys.exit()
+
+a = int(sys.argv[1])
+b = int(sys.argv[2])
+c = int(sys.argv[3])
+d = int(sys.argv[4])
+e = int(sys.argv[5])
+
+marks = [a, b, c, d, e]
+
+if any(m < 35 for m in marks):
+    grade = "F"
+else:
+    total = sum(marks)
+    percentage = total / 5
+
+    if percentage >= 90:
+        grade = "A"
+    elif percentage >= 75:
+        grade = "B"
+    elif percentage >= 60:
+        grade = "C"
+    else:
+        grade = "D"
+
+print("Total Marks:", total)
+print("Percentage:", percentage)
+print("Grade:", grade)
